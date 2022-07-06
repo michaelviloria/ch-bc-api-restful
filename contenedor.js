4db596d191ab633f0e27cedd7eaf148de79e31f7
= <michaelviloria.g@gmail.com>
2022-07-06 09:58:12 -0500

    optimizacion al proceso de eliminar productos

--- contenedor.js
+++ contenedor.js
@@ -1,56 +1,140 @@
 const fs = require("fs");
 
 module.exports = class Contenedor {
-  constructor(nombreArchivo) {
-    this.nombreArchivo = nombreArchivo;
-  }
+	constructor(nameFile) {
+		this.nameFile = nameFile;
+	}
 
-  getAll() {
-    const respuesta = fs.readFileSync(this.nombreArchivo, "utf-8");
-    return (JSON.parse(respuesta));
-  }
-  
-  get(id) {
-    const datos = this.getAll();
-    if(id <= 0 || id > datos.length) {
-      return { error: "El producto con el id especificado no ha sido encontrado."}
+	getAll() {
+		const response = fs.readFileSync(this.nameFile, "utf-8");
+    if(response === "") {
+			return this.assign(true);
+    } else {
+      return JSON.parse(response);
     }
-    return datos[id - 1];
-  }
+	}
 
-  save(product) {
-    const datos = this.getAll();
-    product.id = datos.length + 1;
-    datos.push(product);
-    fs.writeFileSync(this.nombreArchivo, JSON.stringify(datos));
-    return {
-      product: product
-    }
-  }
+	get(id) {
+		const data = this.getAll();
+		if (id <= 0 || id > data.length) {
+			return {
+				error: "El producto con el id especificado no ha sido encontrado.",
+			};
+		}
+		return data.find(element => element.id === id);
+	}
 
-  update(id, product) {
-    const datos = this.getAll();
-    if(id <= 0 || id > datos.length) {
-      return { error: "El producto con el id especificado no ha sido encontrado."}
-    }
-    product.id = id;
-    const previousProduct = datos.splice(id - 1, 1, product);
-    fs.writeFileSync(this.nombreArchivo, JSON.stringify(datos));
-    return {
-      previous: previousProduct,
-      new: product
-    }
-  }
+	save(product) {
+		const data = this.getAll();
+		product.id = data.length + 1;
+		data.push(product);
+		fs.writeFileSync(this.nameFile, JSON.stringify(data));
+		return {
+			product: product,
+		};
+	}
 
-  delete(id) {
-    const datos = this.getAll();
-    if(id <= 0 || id > datos.length) {
-      return { error: "El producto con el id especificado no ha sido encontrado."}
-    }
-    const previousProduct = datos.splice(id - 1, 1);
-    fs.writeFileSync(this.nombreArchivo, JSON.stringify(datos));
-    return {
-      deleted: previousProduct
-    }
-  }
+	update(id, product) {
+		const data = this.getAll();
+		if (id <= 0 || id > data.length) {
+			return {
+				error: "El producto con el id especificado no ha sido encontrado.",
+			};
+		}
+		product.id = id;
+		const previousProduct = data.splice(id - 1, 1, product);
+		fs.writeFileSync(this.nameFile, JSON.stringify(data));
+		return {
+			previous: previousProduct,
+			new: product,
+		};
+	}
+
+	delete(id) {
+		const data = this.getAll();
+		if (id <= 0 || id > data.length) {
+			return {
+				error: "El producto con el id especificado no ha sido encontrado.",
+			};
+		} else {
+			const previousProduct = data.splice(id - 1, 1);
+			fs.writeFileSync(this.nameFile, JSON.stringify(data));
+			this.assign();
+			return {
+				deleted: previousProduct,
+			}
+		}
+	}
+
+  // Agrega id a los productos del archivo "productos.json" si sufre alguna modificacion
+  assign(empty = false) {
+		if(empty) {
+			let id = 1;
+			listaProductos.forEach(element => {
+				element.id = id++;
+			});
+			fs.writeFileSync(this.nameFile, JSON.stringify(listaProductos));
+			return listaProductos;
+		} else {
+			const data = this.getAll();
+			let id = 1;
+			data.forEach(element => {
+				element.id = id++;
+			});
+			fs.writeFileSync(this.nameFile, JSON.stringify(data));
+		}
+	}
 }
+
+const listaProductos = [
+	{
+		nombre: "Nike Jordan Style Loyal",
+		precio: 450000,
+		imagen: "https://i.ibb.co/SK18Szh/Nike-Jordan-Stay-Loyal.webp",
+	},
+	{
+		nombre: "Adidas Stan Smith",
+		precio: 450000,
+		imagen: "https://i.ibb.co/C6y979J/Stan-Smith.webp",
+	},
+	{
+		nombre: "Puma Electrify Nitro",
+		precio: 450000,
+		imagen: "https://i.ibb.co/5kjhkmP/Electrify-Nitro.webp",
+	},
+	{
+		nombre: "Forum 84 Low ADV",
+		precio: 45000,
+		imagen: "https://i.ibb.co/VxWk8j9/Forum-84-Low-ADV.webp",
+	},
+	{
+		nombre: "Retropy F2",
+		precio: 45000,
+		imagen: "https://i.ibb.co/YyfNVFK/Retropy-F2.webp",
+	},
+	{
+		nombre: "Forum Mid",
+		precio: 45000,
+		imagen: "https://i.ibb.co/2tfJ0wR/Forum-Mid.jpg",
+	},
+	{
+		nombre: "OZWEEGO Celox",
+		precio: 45000,
+		imagen: "https://i.ibb.co/9gHwV04/OZWEEGO-Celox.webp",
+	},
+	{
+		nombre: "Kosmo Rider Gradient",
+		precio: 45000,
+		imagen: "https://i.ibb.co/WW59BSF/Kosmo-Rider-Gradient.webp",
+	},
+	{
+		nombre: "Liberate Nitro",
+		precio: 45000,
+		imagen: "https://i.ibb.co/hm8rQ57/Liberate-Nitro.webp",
+	},
+	{
+		nombre: "Nike Jordan Stay Loyal",
+		precio: 45000,
+		imagen: "https://i.ibb.co/SK18Szh/Nike-Jordan-Stay-Loyal.webp",
+	}
+];
